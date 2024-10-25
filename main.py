d418478a57da396e80e304ba4ebd6b753aeed403
Frank <MeridaFranSiles@hotmail.com>
2024-10-24 19:19:40 -0600

    Se agrego menú, división falla en especial con num negativos

---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+from DeciToBin import *
+from negación import *
+from sumaBin import *
+from resta import *
+from multipliBi import *
+from divisionBi import *
+from Com2toDeci import *
+
+def pedir_numero_entero():
+    while True:
+        try:
+            # Pedir al usuario un valor y convertirlo en entero
+            numero = int(input("Por favor, ingresa un número entero: "))
+            return numero
+        except ValueError:
+            # Si no es un número entero, mostrar un mensaje de error
+            print("Error: No es un número entero válido. Intenta de nuevo.")
+
+def menu():
+    print("Ingrese dos números enteros que desea convertir en binario")
+    num1 = pedir_numero_entero()
+    num2 = pedir_numero_entero()
+    # if not num1 or not num2: # Si está vacía, pedir un nuevo valor al usuario
+    #     num1 = pedir_numero_entero()
+    #     num2 = pedir_numero_entero()
+    # else:
+    #     #Si ya contiene valor
+    #     print(f"El primer número es {num1} y el segúndo número es {num2}")
+    #     respuesta = input("¿Deseas seguir usando este valor? (S/n): ").strip().lower()
+    #     if respuesta == "n":
+    #         num1 = pedir_numero_entero()
+    #         num2 = pedir_numero_entero()
+
+    PrimerBi =DecBin(num1)
+    print(f" El primer binario es: {PrimerBi}")
+    SegunBi =DecBin(num2)
+    print(f" El segundo binario es: {SegunBi}")
+    print("\n \n")
+    print("¿Qué operación desea realizar?")
+    print("(1) Suma  (2) Resta  (3) Multiplicación (4) División")
+    opcion = int(input("Elija una opción :"))
+
+    if (opcion == 1):
+        respuesta = sumaBinaria(PrimerBi,SegunBi)
+        print(f"La repuesta es: {respuesta}")
+        aux= binDec(respuesta)
+        print(f"En decimal es: {aux}")
+
+    if (opcion == 2):
+        respuesta = restaBinaria(PrimerBi,SegunBi)
+        print(f"La repuesta es: {respuesta}")
+        aux= binDec(respuesta)
+        print(f"En decimal es: {aux}")
+
+    if (opcion == 3):
+        respuesta = multiBi(PrimerBi,SegunBi)
+        print(f"La repuesta es: {respuesta}")
+        aux= binDec(respuesta)
+        print(f"En decimal es: {aux}")
+
+    if (opcion == 4):
+        resto, cociente = divi(PrimerBi,SegunBi)
+        print(f"El resto de : {resto} y el cociente es {cociente}")
+        cociDeci= binDec(cociente)
+        print(f"el cociente es : {cociDeci}")
+        resDeci= binDec(resto)
+        print(f"el resto es : {resDeci}")
+
+
+print("\nCalculadora de números binarios\n")
+control = True
+# Inicializamos las variables fuera del ciclo
+while control:
+    
+    # num1 = None
+    # num2 = None
+    menu()
+    op = input("¿Desea continuar? (S) Salir,  (C) Continuar ").strip().lower()
+    if op == "s":
+        control = False
+    else:
+        os.system("clear")
+        
--- multipliBi.py
+++ multipliBi.py
@@ -12,36 +12,52 @@ def multiBi(numBi1, numBi2):
     regisM = numBi2
     bitQm1 = "0"
     contador = len(numBi1)
+    print(f"El registro A sera {regisA}")
+    print(f"El multiplicando es: {regisM}")
+    print(f"El multiplicador es: {regisQ}")
+    print(f"El registro Q-1 es:; {bitQm1}")
+    print(f"El contador empieza en: {contador}")
     #empezamos ciclo          NOTA: los desplazamientos se hacen a la DERECHA 
     for i in range(len(numBi1)):
         aux = bitQm1
         comprobación = regisQ[-1] + aux
+        print("\n")
+        print(f"Contador {contador}")
+        print(f"QoQ-1 es igual a {comprobación}")
         if (comprobación == "01"):
+            print("suma A <-- A + M")
             regisA = sumaBinaria(regisA,regisM) #suma A <-- A + M
+            print("Desplazamiento aritmético: A, Q, Q-1")
             #dezplazar
             bitQm1 = regisQ[-1]
             regisQ = regisQ[:-1]
             regisQ = regisA[-1] + regisQ #Agrega al Inicio
             regisA = regisA[:-1]# Elimina el ultimo carácter
             regisA = "1" + regisA if regisA[0] == "1" else "0" + regisA #rellenar A
+            print(f"A{regisA}, Q{regisQ}, Q-1{bitQm1}")
             # regisA = "0" + regisA
         elif (comprobación == "10"): #caso resta y desplazar. Llamar al negado para opera
             #resta A <-- A - M (se necesita el negado de M)
+            print("resta A <-- A - M")
             negaM = nega(regisM)
             regisA = sumaBinaria(regisA,negaM) #Pasa de lista a cadena
+            print("Desplazamiento aritmético: A, Q, Q-1")
             #dezplazar
             bitQm1 = regisQ[-1]
             regisQ = regisQ[:-1]
             regisQ = regisA[-1] + regisQ #Agrega al Inicio
             regisA = regisA[:-1]# Elimina el ultimo carácter
             regisA = "1" + regisA if regisA[0] == "1" else "0" + regisA #rellenar A
+            print(f"A{regisA}, Q{regisQ}, Q-1{bitQm1}")
         else:           #Caso directo a desplazar
+            print("Desplazamiento aritmético: A, Q, Q-1")
             #dezplazar
             bitQm1 = regisQ[-1]
             regisQ = regisQ[:-1]
             regisQ = regisA[-1] + regisQ #Agrega al Inicio
             regisA = regisA[:-1]# Elimina el ultimo carácter
             regisA = "1" + regisA if regisA[0] == "1" else "0" + regisA #rellenar A
+            print(f"A{regisA}, Q{regisQ}, Q-1{bitQm1}")
         
         contador -= 1
         
